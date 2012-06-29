c5bf2fe058fad21341e620fbddc733ebc1a9333b
Avi Romanoff <aviromanoff@gmail.com>
2012-06-28 20:42:58 -0400

    implement some business logic

--- src/main/java/com/example/Aeserv.java
+++ src/main/java/com/example/Aeserv.java
@@ -21,22 +21,28 @@ public class Aeserv {
       post (new Route("/new") {
          @Override
          public Object handle (Request request, Response response) {
-            return "Username was: " + request.queryParams("from") + "\n\nWhat you want, baby I got it: " + request.body();
+            try {
+               saveMessage(request.queryParams("to"), request.body());
+               response.status(200);
+               return "OK";
+            } else {
+               e.printStackTrace();
+               response.status(500);
+               return "FAIL";
+            }
          }
       });
 
       try {
          conn = getConnection();
          createTables();
-         Statement st = conn.createStatement();
-         Boolean res = st.execute("INSERT INTO cities VALUES ('derp')");
-         st = conn.createStatement();
-         ResultSet rs = st.executeQuery("SELECT * FROM cities");
-         while (rs.next()) {
-             System.out.print("Column 1 returned ");
-             System.out.println(rs.getString(1));
-         }
-         rs.close();
+         // st = conn.createStatement();
+         // ResultSet rs = st.executeQuery("SELECT * FROM cities");
+         // while (rs.next()) {
+         //     System.out.print("Column 1 returned ");
+         //     System.out.println(rs.getString(1));
+         // }
+         // rs.close();
       } catch (Exception e) {
          e.printStackTrace();
          System.exit(1);
@@ -46,15 +52,19 @@ public class Aeserv {
 
    static void createTables () throws URISyntaxException, SQLException {
       Statement st = conn.createStatement();
-      st.execute("CREATE TABLE messages (user varchar(80), message text);");
+      st.execute("CREATE TABLE IF NOT EXISTS messages (user varchar(80), message text);");
    }
 
-   static void saveMessage (String message) {
-
+   static void saveMessage (String user, String message) throws SQLException {
+      Statement st = conn.createStatement();
+      PreparedStatement ps = conn.prepareStatement("INSERT INTO messages VALUES (?, ?)");
+      ps.setString(1, user);
+      ps.setString(2, message);
+      ps.executeQuery();
    }
 
    static void readMessages (String user) {
-      
+
    }
 
    static Connection getConnection () throws URISyntaxException, SQLException {
